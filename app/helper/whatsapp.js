5cd4248c9cc1f968ce43d1d55fa81a88cdff26fa
Miftah Steven <miftahsteven@gmail.com>
2024-07-05 10:24:56 +0700

    ada perubahan pada whatsapp

--- app/helper/whatsapp.js
+++ app/helper/whatsapp.js
@@ -9,16 +9,16 @@ const sendWhatsapp = async ({ wa_number, text }) => {
 
   let config = {
     method: 'post',
-    url: 'https://erpapi.zisindosat.id/v1/messages',
+    url: 'https://erpapi.zisindosat.id/wapi/send_message',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer secret'
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiIxMjM0NTY3ODkiLCJlbWFpbCI6IjEyMzQ1Njc4OUB6aXNpbmRvc2F0LmlkIiwiaWF0IjoxNzIwMDEzODQ5LCJhdWQiOlsiMTIzNDU2Nzg5Il0sImlzcyI6InozcjBieXRlcyJ9.7Ooh3xUkhG-f_jAPNBCm7LxWV3E751W0JpgTN8ys-Ss'
     },
     httpsAgent: agent,
     data: {
-      "clientId": "ziswaf3",
+      "client_d": "e84b1720148217601",
       // "phone": "6289657528745",
-      "phone": wa_number,
+      "number": wa_number,
       "message": text,
     }
   };
