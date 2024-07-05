5641e1de3d631f07434c0dda25c4bdc56abef7f1
kedarnacha <kedaranugerahnachaputra@gmail.com>
2024-07-05 11:21:33 +0700

    fixing

--- app/helper/whatsapp.js
+++ app/helper/whatsapp.js
@@ -9,30 +9,30 @@ const sendWhatsapp = async ({ wa_number, text }) => {
 
   let config = {
     method: 'post',
-    url: 'https://erpapi.zisindosat.id/v1/messages',
+    url: 'https://erpapi.zisindosat.id/wapi/send_message',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer secret'
     },
     httpsAgent: agent,
     data: {
-      "clientId": "ziswaf3",
+      "clientId": "e84b1720148217601",
       // "phone": "6289657528745",
-      "phone": wa_number,
+      "number": wa_number,
       "message": text,
     }
   };
 
-  // const send = await axios.request(config)
-  // .then((response) => {
-  //   console.log("----> WA SEND", JSON.stringify(response.data));
-  // })
-  // .catch((error) => {
-  //   console.log(error);
-  // });
+  const send = await axios.request(config)
+  .then((response) => {
+    console.log("----> WA SEND", JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-  // return send;
-  return true;
+  return send;
+  // return true;
 };
 
 
