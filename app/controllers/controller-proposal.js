da19b9524e0461cfe52bb18b27cec06e30318ce2
Miftah Steven <miftahsteven@gmail.com>
2024-07-12 15:45:27 +0700

    perbaikan pada doneProposal

--- app/controllers/controller-proposal.js
+++ app/controllers/controller-proposal.js
@@ -540,6 +540,130 @@ module.exports = {
     }
   },
 
+
+  async doneProposalRev(req, res) {
+    try {
+      const id = req.params.id
+      const ispaid = req.body.ispaid;
+      const nama = req.body.nama;
+      const ref = req.body.ref;
+      const tgl_bayar = new Date()
+
+      let imkas = proposalss.nomor_imkas
+      if (imkas.substring(0, 1) == '0') {
+        imkas = "0" + imkas.substring(1).trim()
+      } else if (imkas.substring(0, 3) == '+62') {
+        imkas = "0" + imkas.substring(3).trim()
+      }
+
+      const checkDataImkas = prisma.trans_imkas.findUnique({
+        where : {
+            imkas_ref_number : Number(id),
+            imkas_no: imkas.replace(/[^0-9\.]+/g, "")
+        }
+      })
+
+      if(checkDataImkas > 0){
+
+      }else{
+
+        const proposalss = await prisma.proposal.findUnique({
+          where: {
+            id: Number(id),
+          },
+        })
+        const userss = await prisma.user.findUnique({
+          where: {
+            user_id: proposalss.user_id,
+          },
+          include: {
+            mustahiq: true,
+          },
+        })
+
+
+        const check = await sendImkas({
+          phone: imkas.replace(/[^0-9\.]+/g, ""),
+          nom: proposalss.dana_yang_disetujui,
+          id: id,
+          desc: "Dana telah dikirimkan",
+        });
+        console.log(check);
+  
+        if (check.responseCode != '00') {
+          return res.status(400).json({ message: check.responseDescription });
+        }
+  
+        if (check.responseCode == '00') {
+  
+          const proposal = await prisma.proposal.update({
+            where: {
+              id: Number(id),
+            },
+            data: {
+              ispaid,
+              tgl_bayar
+            },
+            include: {
+              user: {
+                select: {
+                  mustahiq: true
+                }
+              }
+            }
+          });
+
+          const currentDate = new Date();
+          const formattedDate = currentDate.toLocaleDateString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' });
+
+          if (!proposal) {
+            return res.status(400).json({
+              message: "Proposal tidak ditemukan",
+            });
+          }
+
+          if (ispaid == 1) {
+
+            let pn = ref
+            if (pn.substring(0, 1) == '0') {
+              pn = "0" + pn.substring(1).trim()
+            } else if (pn.substring(0, 3) == '+62') {
+              pn = "0" + pn.substring(3).trim()
+            }
+
+            const formattedDana = proposal.dana_yang_disetujui.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' });
+
+            const msgId = await sendWhatsapp({
+              wa_number: pn.replace(/[^0-9\.]+/g, ""),
+              text: `Proposal Atas Nama ${nama} telah disetujui dan telah ditransfer pada ${formattedDate} sejumlah ${formattedDana} ke nomor IMKas ${proposal.user.mustahiq.imkas_number} atau Rekening ${proposal.user.mustahiq.bank_number} a.n ${proposal.user.mustahiq.bank_account_name} anda. Terima kasih`,
+            });
+          }
+
+
+      }
+
+      
+
+      // let imkas = proposalss.nomor_imkas
+      // if (imkas.substring(0, 1) == '0') {
+      //   imkas = "0" + imkas.substring(1).trim()
+      // } else if (imkas.substring(0, 3) == '+62') {
+      //   imkas = "0" + imkas.substring(3).trim()
+      // }      
+
+        
+      }
+      return res.status(200).json({
+        message: "Sukses",
+        data: "Berhasil Ubah Data",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: error?.message,
+      });
+    }
+  },
+
   //////////////////
   async updateProposal(req, res) {
     try {
