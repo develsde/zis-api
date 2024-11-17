b20298d980a3ac0b5194c79d8625c078aac2f146
Icunsuwekrek <ikhsansani226@gmail.com>
2024-11-17 12:40:58 +0700

    update laporan

--- app/controllers/controller-home.js
+++ app/controllers/controller-home.js
@@ -3031,7 +3031,7 @@ module.exports = {
       const page = Number(req.query.page || 1);
       const perPage = Number(req.query.perPage || 10);
       const skip = (page - 1) * perPage;
-
+  
       const params = {
         OR: [
           {
@@ -3046,7 +3046,7 @@ module.exports = {
           },
         ]
       };
-
+  
       const [count, result] = await prisma.$transaction([
         prisma.affiliator.count({
           where: params,
@@ -3063,27 +3063,26 @@ module.exports = {
               },
             },
           },
-          skip,
-          take: perPage,
         }),
       ]);
-
-      const affiliatorsWithTotalHarga = result.map((affiliator) => {
-        const totalHarga = affiliator.pemesanan_megakonser.reduce(
-          (sum, order) => sum + order.total_harga,
-          0
-        );
-
-        return {
-          nama: affiliator.nama,
-          kode: affiliator.kode,
-          total_harga: totalHarga,
-        };
-      });
-
-      // Urutkan hasil berdasarkan total_harga secara menurun
-      affiliatorsWithTotalHarga.sort((a, b) => b.total_harga - a.total_harga);
-
+  
+      // Hitung totalHarga dan urutkan dari terbesar ke terkecil berdasarkan totalHarga
+      const affiliatorsWithTotalHarga = result
+        .map((affiliator) => {
+          const totalHarga = affiliator.pemesanan_megakonser.reduce(
+            (sum, order) => sum + order.total_harga,
+            0
+          );
+  
+          return {
+            nama: affiliator.nama,
+            kode: affiliator.kode,
+            total_harga: totalHarga,
+          };
+        })
+        .sort((a, b) => b.total_harga - a.total_harga) // Urutkan berdasarkan total_harga dari terbesar ke terkecil
+        .slice(skip, skip + perPage); // Paginate setelah sorting
+  
       res.status(200).json({
         message: "Sukses Ambil Data",
         data: affiliatorsWithTotalHarga,
@@ -3100,6 +3099,7 @@ module.exports = {
       });
     }
   },
+  
 
   async getAffiliator(req, res) {
     try {
--- app/routes/route-report.js
+++ app/routes/route-report.js
@@ -5,6 +5,6 @@ const { authentication, authorization } = require("../../config/auth");
 const { upload } = require("../helper/upload");
 
 // GET localhost:8080/home => Ambil data semua dari awal
-router.get("/create-report", authentication, report.createReport);
+router.post("/create-report", authentication, report.createReport);
 
 module.exports = router;
